6a9c0b27afdeca736ade898cd55de66681369cc6
Marcelino Pangilinan <marbysneaker@gmail.com>
2023-10-06 21:12:02 -0700

    change routes

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -1,18 +1,9 @@
 import express from 'express'
-import path from 'path'
-
-
-import { fileURLToPath } from 'url'
-import giftData from '../data/gifts.js';
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import GiftsController from '../controllers/gifts.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.status(200).json(giftData)
-  })
+router.get('/', GiftsController.getGifts)
 
 router.get('/:giftId', GiftsController.getGiftById)
 
